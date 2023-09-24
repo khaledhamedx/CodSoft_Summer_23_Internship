185b7ed8a33ce589789b562831c86d72b725dc72
Khaled Ahmed Hamed <145912120+khaledhamedx@users.noreply.github.com>
2023-09-24 21:33:33 +0300

    Update XO.cpp

--- Task 3 - XO Game/XO.cpp
+++ Task 3 - XO Game/XO.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
 #include <iostream>
-/* 
-#define BLACK_TEXT "\033[30m"
-#define RED_TEXT "\033[31m"
-#define GREEN_TEXT "\033[32m"
-#define YELLOW_TEXT "\033[33m"
-#define BLUE_TEXT "\033[34m"
-#define MAGENTA_TEXT "\033[35m"
-#define CYAN_TEXT "\033[36m"
-#define WHITE_TEXT "\033[37m"
-*/
 using namespace std;
 int checkwin();
 void grid();
